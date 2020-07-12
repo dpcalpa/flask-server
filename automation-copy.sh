03997b771e1c6bb6d5dd2b72d47f7deac04205c5
Diana Calpa <dp.calpa251@gmail.com>
2020-07-12 09:56:06 -0500

    New change on 2020-07-12

--- automation-copy.sh
+++ automation-copy.sh
@@ -7,6 +7,6 @@ git status
 echo "---------------------- git add --all"
 git add --all
 echo "---------------------- git commit -m"
-git commit -m "`date +\"%Y-%m-%d\"`"
+git commit -m "New change on `date +\"%Y-%m-%d\"`"
 echo "---------------------- git push"
 git push 
