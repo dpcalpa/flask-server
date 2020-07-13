88406630b7eef5afcfc8b178fed43b96bfd035b2
Diana Calpa <dp.calpa251@gmail.com>
2020-07-12 22:04:06 -0500

    New change on 2020-07-12 22:04:06

--- automation.sh
+++ automation.sh
@@ -7,14 +7,6 @@
 #• Smoke test
 
 DOCKER="python-app"
-echo "---------------------- git status"
-git status
-echo "---------------------- git add --all"
-git add --all
-echo "---------------------- git commit -m"
-git commit -m "New change on `date +\"%Y-%m-%d %H:%M:%S\"`"
-echo "---------------------- git push"
-git push origin master
 
 #Clone repository
 git clone https://github.com/dpcalpa/flask-server.git
