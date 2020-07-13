606ba1308a032c6a0f2ff11f1700197c6bbff7bf
Diana Calpa <dp.calpa251@gmail.com>
2020-07-12 22:02:52 -0500

    New change on 2020-07-12 22:02:52

--- automation.sh
+++ automation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 #• Build
-#• Local unit or integration tests
+#• Local unit tests
 #• Package
 #• Deploy
 #• Smoke test
@@ -15,20 +16,37 @@ git commit -m "New change on `date +\"%Y-%m-%d %H:%M:%S\"`"
 echo "---------------------- git push"
 git push origin master
 
-#Get to repository? Clone, download?
+#Clone repository
+git clone https://github.com/dpcalpa/flask-server.git
 
-#Make unit tests to app.py
+#Install dependencies locally assuming you have Mac machine and brew installed
+#Python 
+brew install python
+#Pip
+sudo easy_install pip
+#Flask
+pip3 install -r requirements.txt
+
+#Run unit tests to app.py
 python app/tests/test.py
 
 #Make docker build to get image python-app:latest
 docker build -t $DOCKER:latest .
 echo "BUILD done"
+echo "IMAGE $DOCKER:latest listed"
 docker container ls
 
 #Apply Kubernetes configuration
 kubectl apply -f yaml-config/
 echo "KUBERNETES CONFIGURATION applied"
+echo "KUBERNETES RESOURCES listed: "
 kubectl get all
 
+#Wait until kubernetes deploys configuration
+sleep 10
+
+#Smoke test
 echo "Test app with: curl -ikL http://localhost/home"
+curl -ikL http://localhost/home
 echo "Test app with: curl -ikL http://localhost/health"
+curl -ikL http://localhost/health
